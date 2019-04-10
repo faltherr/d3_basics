83a6700518c18e28a767b8d9afb8aaa96bf68bef
Forest Altherr <faltherr1@gmail.com>
2019-04-10 11:30:50 -0600

    Using data in an external json file along with .enter to render to dom based on data.

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-// d3 expects data in an array format
-const data = [
-    {width:200,height:100, fill:'purple'},
-    {width:100,height:60, fill:'pink'},
-    {width:50,height:30, fill:'red'}
-]
+// select svg container first
 
-// get a reference to the svg first almost always
 const svg = d3.select('svg')
 
-// Join data to rects
-const rects = svg.selectAll('rect')
-    // join data to the element
-    // on the rect's props there is a __data__ that is given the data we specified
-    .data(data)
+// This is an asynchronous function so it returns a promise
+d3.json('planets.json').then( data => {
 
-// Add attrs to rects ALREADY in the dom
-rects.attr('width', (d, i, n) => d.width)
-    .attr('height', d => d.height)
-    .attr('fill', d => d.fill);
+    const circles = svg.selectAll('circle')
+        .data(data);
+    // Add attrs to circles already in dom
+    // Update circles in DOM
+    circles.attr('cy', 200)
+        .attr('cx', d => d.distance)
+        .attr('r', d=> d.radius)
+        .attr('fill', d=> d.fill);
 
-
-//d3 sees we have virtual elements that need to be rendered
-//This updates the stuff that has yet to enter the DOM
-// append the enter selection to the dom
-rects.enter()
-    .append('rect')
-    .attr('width', (d, i, n) => d.width)
-    .attr('height', d => d.height)
-    .attr('fill', d => d.fill);
+    // Append the enter selection to the DOM
+    circles.enter()
+        .append('circle')
+        .attr('cy', 200)
+        .attr('cx', d => d.distance)
+        .attr('r', d=> d.radius)
+        .attr('fill', d=> d.fill);
+    //End of the .then
+})
