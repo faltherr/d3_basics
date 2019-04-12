23ce9e5157fc1d5ce63888947f5229b16928d805
Forest Altherr <faltherr1@gmail.com>
2019-04-12 15:15:30 -0600

    Preparing for moving into an update function

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ const graph = svg.append('g')
     // Translate takes an x translation and a y translation value
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
+graph.append('rect');
+graph.append('rect');
+graph.append('rect');
+graph.append('rect');
+graph.append('rect');
+graph.append('rect');
+graph.append('rect');
+
 // We want the axis to be within the graph
 const xAxisGroup = graph.append('g')
 //translate 0 means we don't want to shift the graph in the x direction
@@ -28,7 +36,21 @@ const xAxisGroup = graph.append('g')
     .attr('transform', `translate(0, ${graphHeight})`);
 const yAxisGroup = graph.append('g');
 
-d3.json('menu.json').then(data => {
+// Update function
+const update = (data) => {
+    // Update scale domains if they rely on our data
+    
+}
+
+
+db.collection('dishes').get().then(res => {
+    // console.log(res);
+    let data = [];
+    res.docs.forEach(doc => {
+        // the data method returns the data
+        data.push(doc.data());
+    });
+    // console.log(data);
 
     // height of bar should be the height of the graph minus the total value
     const y = d3.scaleLinear()
@@ -37,9 +59,9 @@ d3.json('menu.json').then(data => {
         //Flip the range from graphHeight to 0 to make the scale go in opposite direction
         .range([graphHeight,0])
 
-        console.log(y(400))
-        console.log(y(0))
-        console.log(y(900))
+        // console.log(y(400))
+        // console.log(y(0))
+        // console.log(y(900))
 
     // // Min takes in the 2 parameters (Data and a function)
     // const min = d3.min(data, d => d.orders)
@@ -60,47 +82,51 @@ d3.json('menu.json').then(data => {
         .paddingOuter(0.2);
 
     // This will give us the location of where the bar should start on the x axis
-    console.log(x('veg curry'))
+    // console.log(x('veg curry'))
     // Get the width of the bar from the bandwidth
-    console.log(x.bandwidth())
+    // console.log(x.bandwidth())
 
     // We need the rectangles to be on the graph (Because the graph is in the svg)
     const rects = graph.selectAll('rect')
         .data(data)
+
+    console.log(rects)
+    // // RECTS.ATTR updates the rectangles ALREADY in the DOM
     // Width is standard, height depends on data
-    rects.attr('width', x.bandwidth)
-        // This calculates the correct height
-        // The bar should start at the value and then extend to the bottom of the chart
-        .attr('height', d => graphHeight - y(d.orders))
-        .attr('fill', 'orange')
-        // Offset the bars calculated by the d3.scaleBand function
-        .attr('x', d => x(d.name))
-        .attr('y', d => y(d.orders));
+    // rects.attr('width', x.bandwidth)
+    //     // This calculates the correct height
+    //     // The bar should start at the value and then extend to the bottom of the chart
+    //     .attr('height', d => graphHeight - y(d.orders))
+    //     .attr('fill', 'orange')
+    //     // Offset the bars calculated by the d3.scaleBand function
+    //     .attr('x', d => x(d.name))
+    //     .attr('y', d => y(d.orders));
 
-    // Append the enter selection to the dom
-    rects.enter()
-        .append('rect')
-        .attr('width', x.bandwidth)
-        .attr('height', d => graphHeight - y(d.orders))
-        .attr('fill', 'orange')
-        .attr('x', d => x(d.name))
-        .attr('y', d => y(d.orders));
+    // // VIRTUAL ELEMENTS
+    // // Append the enter selection to the dom
+    // rects.enter()
+    //     .append('rect')
+    //     .attr('width', x.bandwidth)
+    //     .attr('height', d => graphHeight - y(d.orders))
+    //     .attr('fill', 'orange')
+    //     .attr('x', d => x(d.name))
+    //     .attr('y', d => y(d.orders));
     
-    // create and call axis
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y)
-        .ticks(3)
-        .tickFormat(d => d + ' orders');
+    // // create and call axis
+    // const xAxis = d3.axisBottom(x);
+    // const yAxis = d3.axisLeft(y)
+    //     .ticks(3)
+    //     .tickFormat(d => d + ' orders');
 
-    xAxisGroup.call(xAxis);
-    yAxisGroup.call(yAxis);
+    // xAxisGroup.call(xAxis);
+    // yAxisGroup.call(yAxis);
 
-    // rotate text to -40 degrees
-    xAxisGroup.selectAll('text')
-        //When we rotate we are rotating around the text anchor
-        // So we need to make the rotation poitn the end of the text
-        .attr('transform', 'rotate(-40)')
-        .attr('text-anchor', 'end')
-        .attr('fill', 'orange');
+    // // rotate text to -40 degrees
+    // xAxisGroup.selectAll('text')
+    //     //When we rotate we are rotating around the text anchor
+    //     // So we need to make the rotation poitn the end of the text
+    //     .attr('transform', 'rotate(-40)')
+    //     .attr('text-anchor', 'end')
+    //     .attr('fill', 'orange');
 
     });
