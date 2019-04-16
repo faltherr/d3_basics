4f876b18563b43b6695977a4ce39cc5643553764
Forest Altherr <faltherr1@gmail.com>
2019-04-16 14:33:47 -0600

    Simple transitions to bar width and height

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// 1. Steup the graph
+// ** Start Block **
+
 // select svg container first
 // slect the canvas then append a svg
 const svg = d3.select('.canvas')
@@ -21,14 +24,6 @@ const graph = svg.append('g')
     // Translate takes an x translation and a y translation value
     .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-graph.append('rect');
-graph.append('rect');
-graph.append('rect');
-graph.append('rect');
-graph.append('rect');
-graph.append('rect');
-graph.append('rect');
-
 // We want the axis to be within the graph
 const xAxisGroup = graph.append('g')
 //translate 0 means we don't want to shift the graph in the x direction
@@ -36,97 +31,129 @@ const xAxisGroup = graph.append('g')
     .attr('transform', `translate(0, ${graphHeight})`);
 const yAxisGroup = graph.append('g');
 
-// Update function
+// Scales
+const x = d3.scaleBand()
+    .range([0,500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
+
+const y = d3.scaleLinear()
+    .range([graphHeight,0])
+
+// create  axis
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d + ' orders');
+
+// rotate text to -40 degrees
+    xAxisGroup.selectAll('text')
+        //When we rotate we are rotating around the text anchor
+        // So we need to make the rotation poitn the end of the text
+        .attr('transform', 'rotate(-40)')
+        .attr('text-anchor', 'end')
+        .attr('fill', 'orange');
+
+// ** End Block **
+
+// 2. Draw the stuff on the graph which depends on the data
+
+// ** Start Block **
+
+const t = d3.transition().duration(1500)
+
+// UPDATE function
 const update = (data) => {
-    // Update scale domains if they rely on our data
-    
-}
+    // UPDATE scale domains (Only part of scale that depends on data)
+    // Take the scale and 
+    x.domain(data.map(item => item.name))
+    y.domain([0, d3.max(data, d => d.orders)])
 
-
-db.collection('dishes').get().then(res => {
-    // console.log(res);
-    let data = [];
-    res.docs.forEach(doc => {
-        // the data method returns the data
-        data.push(doc.data());
-    });
-    // console.log(data);
-
-    // height of bar should be the height of the graph minus the total value
-    const y = d3.scaleLinear()
-    // max is dynamically calculated
-        .domain([0, d3.max(data, d => d.orders)])
-        //Flip the range from graphHeight to 0 to make the scale go in opposite direction
-        .range([graphHeight,0])
-
-        // console.log(y(400))
-        // console.log(y(0))
-        // console.log(y(900))
-
-    // // Min takes in the 2 parameters (Data and a function)
-    // const min = d3.min(data, d => d.orders)
-    // console.log("min", min)
-    // const max = d3.max(data, d => d.orders)
-    // console.log('max', max)
-    // // Extent finds the min and the maz
-    // // Returns an array
-    // const extent = d3.extent(data, d=> d.orders)
-    // console.log('extent', extent)
-    
-    const x = d3.scaleBand()
-        // Array of names for domain from the data object
-        .domain(data.map(item => item.name))
-        // The graph is 500 pixels in width
-        .range([0,500])
-        .paddingInner(0.2)
-        .paddingOuter(0.2);
-
-    // This will give us the location of where the bar should start on the x axis
-    // console.log(x('veg curry'))
-    // Get the width of the bar from the bandwidth
-    // console.log(x.bandwidth())
-
-    // We need the rectangles to be on the graph (Because the graph is in the svg)
+    //Here we join data to the rectangles
     const rects = graph.selectAll('rect')
         .data(data)
 
-    console.log(rects)
-    // // RECTS.ATTR updates the rectangles ALREADY in the DOM
-    // Width is standard, height depends on data
-    // rects.attr('width', x.bandwidth)
-    //     // This calculates the correct height
-    //     // The bar should start at the value and then extend to the bottom of the chart
-    //     .attr('height', d => graphHeight - y(d.orders))
-    //     .attr('fill', 'orange')
-    //     // Offset the bars calculated by the d3.scaleBand function
-    //     .attr('x', d => x(d.name))
-    //     .attr('y', d => y(d.orders));
+    // Remove the virtual element in the exit selection
+    rects.exit().remove()
 
-    // // VIRTUAL ELEMENTS
-    // // Append the enter selection to the dom
-    // rects.enter()
-    //     .append('rect')
-    //     .attr('width', x.bandwidth)
+    // Update the current shapes in the DOM
+    rects.attr('width', x.bandwidth)
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    // .transition(t)
     //     .attr('height', d => graphHeight - y(d.orders))
-    //     .attr('fill', 'orange')
-    //     .attr('x', d => x(d.name))
     //     .attr('y', d => y(d.orders));
     
-    // // create and call axis
-    // const xAxis = d3.axisBottom(x);
-    // const yAxis = d3.axisLeft(y)
-    //     .ticks(3)
-    //     .tickFormat(d => d + ' orders');
+    // VIRTUAL ELEMENTS
+    // Append the enter selection to the dom
+    rects.enter()
+        .append('rect')
+        // The tween determines the starting position
+        // .attr('width', x.bandwidth)
+        // starting height is 0
+        .attr('height', 0)
+        .attr('fill', 'orange')
+        .attr('x', d => x(d.name))
+        // this gives us the bottom of the chart
+        .attr('y', graphHeight)
+        // transition to the values below
+        // t is defined above so that there is a single location for transition
+        
+        // Anything below applies to the current shapes in the dom (update) and the enter 
+        .merge(rects)
+        .transition(t)
+            .attrTween('width', widthTween)
+            .attr('y', d => y(d.orders))
+            .attr('height', d => graphHeight - y(d.orders));
 
-    // xAxisGroup.call(xAxis);
-    // yAxisGroup.call(yAxis);
+    // call axis
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+}
 
-    // // rotate text to -40 degrees
-    // xAxisGroup.selectAll('text')
-    //     //When we rotate we are rotating around the text anchor
-    //     // So we need to make the rotation poitn the end of the text
-    //     .attr('transform', 'rotate(-40)')
-    //     .attr('text-anchor', 'end')
-    //     .attr('fill', 'orange');
+// ** End Block **
 
+// 3. Get data and run the update for the first time
+
+// Decalre data so that we don't need to adapt to the change
+data = []
+
+//Snapshot looks for changes to the collection 
+db.collection('dishes').onSnapshot(res =>{
+    res.docChanges().forEach(change => {
+        // Grab the data off of the doc reference
+        console.log(change.doc.data())
+        const doc = {...change.doc.data(), id:change.doc.id}
+
+        // Update data based on the change that has occured
+        switch(change.type){
+            case 'added':
+                data.push(doc);
+                break;
+            case 'modified':
+                const index = data.findIndex(item=> item.id === doc.id)
+                data[index] = doc
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id)
+                break;
+            default:
+                break;
+
+        }
     });
+    update(data)
+})
+
+const widthTween = (d) => {
+    // Define an interpolation that returns a function we call i
+    let i = d3.interpolate(0, x.bandwidth());
+    // return a function that takes in a time ticker represented by duration of the transition
+    // t represents the different states that the transition is in
+    // t = d3.transition().duration(500)
+    // An interpolation is calculated for each different time point in the transition
+    return function(t){
+        // this function returns a number between 0 and x.bandwidth
+        return i(t);
+    }
+}
